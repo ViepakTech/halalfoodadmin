f1f74fcfe8dccf118b77d8e0fd119101c93dda73
ViepakTech <viepaktech@gmail.com>
2013-08-09 08:00:30 -0400

    db wrapper changes

--- src/com/viepak/halalfood/db/IngredientDataUtilityGoogle.java
+++ src/com/viepak/halalfood/db/IngredientDataUtilityGoogle.java
@@ -1,33 +1,72 @@
 package com.viepak.halalfood.db;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
 import com.viepak.halalfood.shared.Ingredient;
+import com.viepak.halalfood.shared.User;
 
 public class IngredientDataUtilityGoogle {
 	
 	private static Entity Get(Ingredient ingredient, Entity ingredientEntity){
-		/*userEntity.setProperty("name", user.getName());
-		userEntity.setProperty("role", user.getRole());
-		userEntity.setProperty("email", user.getEmail());
-		userEntity.setProperty("password", user.getPassword());
-		userEntity.setProperty("phoneNumber", user.getPhoneNumber());
-		userEntity.setProperty("IsActive", user.getIsActive());*/
-		
-		ingredientEntity.setProperty("", ingredient.geteNumber());
+		ingredientEntity.setProperty("name", ingredient.getName());
+		ingredientEntity.setProperty("eNumber", ingredient.geteNumber());
+		ingredientEntity.setProperty("status", ingredient.getStatus());
+		ingredientEntity.setProperty("evaluatedDate", ingredient.getEvaluatedDate());
+		ingredientEntity.setProperty("evaluatedBy", ingredient.getEvaluatedBy());
+		ingredientEntity.setProperty("remarks", ingredient.getRemarks());
 		
 		return ingredientEntity;
+	}
+	
+	private static Ingredient Set(Entity result){
+		Ingredient ingredient = new Ingredient();
+		ingredient.setId((long)result.getKey().getId());
+		ingredient.setName((String)result.getProperty("name"));
+		ingredient.seteNumber((String)result.getProperty("eNumber"));
+		ingredient.setStatus((String)result.getProperty("status"));
+		ingredient.setEvaluatedDate((Date)result.getProperty("evaluatedDate"));
+		ingredient.setEvaluatedBy((long)result.getProperty("evaluatedBy"));
+		ingredient.setRemarks((String)result.getProperty("remarks"));
+		
+		return ingredient;
 	}
 	
 	public static Ingredient SyncUser(Ingredient ingredient){
 		DatastoreService dataStore = DatastoreServiceFactory.getDatastoreService();
 		
 		if(ingredient.getId() > 0){
-			dataStore.put(Get(ingredient,new Entity("User", ingredient.getId())));
+			dataStore.put(Get(ingredient,new Entity("Ingredient", ingredient.getId())));
 		}else{
-			dataStore.put(Get(ingredient,new Entity("User")));
+			dataStore.put(Get(ingredient,new Entity("Ingredient")));
 		}
 		return ingredient;
 	}
+	
+	public static List<Ingredient> GetAllIngredients(){
+		DatastoreService dataStore = DatastoreServiceFactory.getDatastoreService();
+		Query q = new Query("Ingredient");
+		PreparedQuery pq = dataStore.prepare(q);
+		
+		List<Ingredient> ingredients = new ArrayList<Ingredient>();
+		
+		for(Entity result: pq.asIterable()){
+			ingredients.add(Set(result));
+		}
+		
+		return ingredients;
+	}
+
+	public static boolean DeleteIngredient(long id){
+		DatastoreService dataStore = DatastoreServiceFactory.getDatastoreService();
+		dataStore.delete(new Entity("Ingredient", id).getKey());
+		return true;
+	} 
+
 }
